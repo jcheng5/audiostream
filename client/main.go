4f50dbda00134a08f25e387bae1c276eb34c2743
Joe Cheng <joe@rstudio.com>
2016-07-31 12:26:14 -0700

    Client improvements
    - Use much smaller buffer size when copying bytes between body and stdin.
      The large buffer size was preventing read from returning until the
      buffer was full.
    - Don't use goroutine-based asynchronous writing. It adds complexity and
      perhaps YAGNI. Copy synchronously using io.CopyN instead.
    - Collect samples of throughput rates in a ring buffer. Will use this in
      the future to diagnose connection health.

--- client/main.go
+++ client/main.go
@@ -6,12 +6,74 @@ import (
   "net/http"
   "os"
   "os/exec"
+  "time"
 )
+
+type WriterFunc func(p []byte) (int, error)
+
+func (w WriterFunc) Write(p []byte)(int, error) {
+  return w(p)
+}
 
 func installed(prog string) bool {
   cmd := exec.Command("which", prog)
   return cmd.Run() == nil
 }
+
+type Metrics struct {
+  ticker *time.Ticker
+  receiveData chan int
+  entries []float32
+  pos int
+  totalEntries int
+  currentBytes int
+  currentStartTime time.Time
+}
+func NewMetrics(duration time.Duration, maxEntries int) *Metrics {
+  ticker := time.NewTicker(duration)
+
+  m := &Metrics{ticker, make(chan int), make([]float32, maxEntries), -1, 0, 0, time.Now()}
+
+  go func() {
+    for {
+      select {
+      case <-ticker.C:
+        if m.pos < 0 {
+          m.pos = 0
+        } else {
+          secs := float32(time.Now().Sub(m.currentStartTime).Seconds())
+          m.entries[m.pos] = float32(m.currentBytes) / 1024 * 8 / secs
+          log.Printf("%f", m.entries[m.pos])
+          m.pos = (m.pos + 1) % len(m.entries)
+          m.totalEntries += 1
+        }
+        m.currentBytes = 0
+        m.currentStartTime = time.Now()
+      case bytes := <-m.receiveData:
+        m.currentBytes += bytes
+      }
+    }
+  }()
+
+  return m
+}
+func (m *Metrics) Write(p []byte) (int, error) {
+  m.receiveData <- len(p)
+  return len(p), nil
+}
+func (m *Metrics) Entries() []float32 {
+  if m.totalEntries == m.pos {
+    entries := make([]float32, m.pos)
+    copy(entries, m.entries[0:m.pos])
+    return entries
+  } else {
+    entries := make([]float32, len(m.entries))
+    copy(entries, m.entries[m.pos:])
+    copy(entries[len(m.entries[m.pos:]):], m.entries[0:m.pos])
+    return entries
+  }
+}
+
 
 func main() {
   if !checkPrereqs() {
@@ -43,27 +105,13 @@ func main() {
     log.Fatal(err)
   }
 
-  received := make(chan []byte)
-  buf := make([]byte, 1024*1024)
-
-  // Start a goroutine for copying byte slices from "received"
-  // and dropping them into stdin
-  go transfer(received, stdin)
-
-  // This loop receives bytes from the http connection
-  // and writes the results to the "received" channel
-  for {
-    count, err := res.Body.Read(buf)
-    if count > 0 {
-      newSlice := make([]byte, count)
-      copy(newSlice, buf[0:count])
-      received <- newSlice
-    }
-    if err != nil {
-      break
-    }
+  metrics := NewMetrics(1 * time.Second, 3 * 60 * 60)
+  sink := io.MultiWriter(stdin, metrics)
+  for ; err == nil; _, err = io.CopyN(sink, res.Body, 1024) {
   }
-
+  if err != io.EOF {
+    log.Fatal(err)
+  }
 }
 
 func transfer(recv chan []byte, stdin io.WriteCloser) {
